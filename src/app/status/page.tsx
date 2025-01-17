19e8cc88105955b93ca49fbd374f0128496a84df
Rigen Maulana <rygen.dev@gmail.com>
2025-01-17 19:56:18 +0700

    fix

--- src/app/status/page.tsx
+++ src/app/status/page.tsx
@@ -1,12 +1,5 @@
-import { createClient } from "@/utils/supabase/server";
 import Container from "./container";
 
 export default async function Page() {
-  const supabase = await createClient();
-  const { data: consultations } = await supabase
-    .from("consultations")
-    .select("*")
-    .filter("ended_at", "gte", new Date().toISOString());
-
-  return <Container consultations={consultations ?? []} />;
+  return <Container />;
 }
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,9 +1,9 @@
 export const calculateEstimatedTime = (endedAt: string): string => {
   const endTime = new Date(endedAt);
   const currentTime = new Date();
-  const diffInHours = Math.round(
-    (endTime.getTime() - currentTime.getTime()) / (1000 * 60 * 60),
+  const diffInMinutes = Math.round(
+    (endTime.getTime() - currentTime.getTime()) / (1000 * 60),
   );
 
-  return `${diffInHours} Hours`;
+  return `${diffInMinutes} minutes`;
 };
